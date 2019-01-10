27dcd35eea3868898897c7d2619b9333dc42cc78
Jace Richardson <jace@packettide.com>
2019-01-10 08:01:42 -0500

    Updated ENV vars. Extracted hard-coded paths into ENV with fallbacks. Cleaned up output display.

--- devstack.sh
+++ devstack.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
 scriptDirectory=`dirname $(readlink ${BASH_SOURCE[0]})`
+
+# Load our ENV vars.
+cp -n $scriptDirectory/.env.example $scriptDirectory/.env
+source $scriptDirectory/.env
+
+# Backwards compatibility for ENV changes.
+if [[ ! $TLD && $tld ]]; then TLD=${tld}; fi
+if [[ ! $DOCKER_FOLDER ]]; then DOCKER_FOLDER='_docker'; fi
+if [[ ! $SITES_FOLDER ]]; then SITES_FOLDER='/code/sites/'; fi
+if [[ ${SITES_FOLDER: -1} != '/' ]]; then SITES_FOLDER="${SITES_FOLDER}/"; fi
+if [[ ! $MYSQL_PORTS_FILE ]]; then MYSQL_PORTS_FILE='/code/docker_mysql_ports.json'; fi
+
 varpwd=`pwd`
-project=${varpwd#/code/sites/}
+project=${varpwd#${SITES_FOLDER}}
 php_versions="5.6, 7.0, 7.1, 7.2"
 mysql_versions="5.5, 5.6, 5.7"
 server_types="[n]ginx, [a]pache"
 ngrok_id=""
-active_file="${varpwd}/_docker/.active"
-public_file="${varpwd}/_docker/.public"
-server_file="${varpwd}/_docker/.server"
+active_file="${varpwd}/${DOCKER_FOLDER}/.active"
+public_file="${varpwd}/${DOCKER_FOLDER}/.public"
+server_file="${varpwd}/${DOCKER_FOLDER}/.server"
 action=''
 forcerestart='0'
 no_ansi=''
 
-cp -n $scriptDirectory/.env.example $scriptDirectory/.env
-source $scriptDirectory/.env
-
 bold=$(tput bold)
 normal=$(tput sgr0)
+
+# Remove the leading slash if present.
+if [[ ${project:0:1} == '/' ]]; then project=${project:1}; fi
 
 php_apache_repos=(
     '5.3::bylexus/apache-php53;;latest'
@@ -44,8 +56,8 @@ php_extensions=(
 )
 
 # Make sure Docker is installed for this project
-if [ ! -d _docker ]; then
-	mkdir -p _docker
+if [ ! -d ${DOCKER_FOLDER} ]; then
+	mkdir -p ${DOCKER_FOLDER}
 fi
 
 if [ "$1" = "restart" ]; then
@@ -69,8 +81,8 @@ done
 # If we don't have a current server file set already, create one.
 if [[ ! -f $server_file ]]
 then
-    touch ${varpwd}/_docker/.server
-    echo "nginx" > ${varpwd}/_docker/.server
+    touch ${varpwd}/${DOCKER_FOLDER}/.server
+    echo "nginx" > ${varpwd}/${DOCKER_FOLDER}/.server
 fi
 
 # Read the server file. It should always exist at this point.
@@ -99,7 +111,7 @@ then
 		# If the first argument is stop, just bring the container down (i.e. "devstack stop").
 		if [ "$1" = "stop" ]; then
 			echo "Shutting down..."
-			docker-compose -p '${project}' -f ${varpwd}/_docker/docker-compose.yml ${no_ansi} stop;
+			docker-compose -p '${project}' -f ${varpwd}/${DOCKER_FOLDER}/docker-compose.yml ${no_ansi} stop;
 			echo "Stack shut down!"
 			echo "---------------------------------------------"
 			printf "\n"
@@ -131,11 +143,11 @@ then
 	then
 		echo "Shutting down..."
 
-		docker-compose -p '${project}' -f ${varpwd}/_docker/docker-compose.yml stop
+		docker-compose -p '${project}' -f ${varpwd}/${DOCKER_FOLDER}/docker-compose.yml stop
 		echo "Stack shut down"
 
 		# Truncate our active file so we don't misreport a running stack.
-		# echo '' > ${varpwd}/_docker/.active
+		# echo '' > ${varpwd}/${DOCKER_FOLDER}/.active
 		exit
 	else
 		echo "FAILED: No stack currently running."
@@ -148,7 +160,7 @@ then
 	shopt -u nocasematch
 
 	# Create our .public file if it doesn't exist
-	touch ${varpwd}/_docker/.public
+	touch ${varpwd}/${DOCKER_FOLDER}/.public
 	current_public_folder=$(<${public_file})
 
 	if [[ -z $current_public_folder ]]
@@ -204,21 +216,21 @@ then
 			public_folder=""
 		fi
 
-		echo ${public_folder} > ${varpwd}/_docker/.public
+		echo ${public_folder} > ${varpwd}/${DOCKER_FOLDER}/.public
 	else
 		public_folder=${current_public_folder}
 	fi
 
 	if [[ $public_folder ]]
 	then
-		public_folder_path="/code/sites/"${project}"/"${public_folder}
+		public_folder_path="${SITES_FOLDER}"${project}"/"${public_folder}
 	else
-		public_folder_path="/code/sites/"${project}
+		public_folder_path="${SITES_FOLDER}"${project}
 	fi
 
 	if [[ $use_ngrok == 'y' ]]
 	then
-		osascript -e 'tell application "Terminal" to do script "ngrok http '${project}'.'${tld}':80"'
+		osascript -e 'tell application "Terminal" to do script "ngrok http '${project}'.'${TLD}':80"'
 	fi
 
 	if [[ $use_ngrok == 'y' || $use_ngrok == 'e' ]]
@@ -314,22 +326,21 @@ if [[ ! -z $running ]]
 then
 	echo "Shutting down..."
 
-	docker-compose -p '${project}' -f ${varpwd}/_docker/docker-compose.yml stop
+	docker-compose -p '${project}' -f ${varpwd}/${DOCKER_FOLDER}/docker-compose.yml stop
 	echo "Stack shut down"
 	echo "---------------------------------------------"
 fi
 
 # Create our active file and docker_mysql_ports file if it doesn't exist
-touch ${varpwd}/_docker/.active
-touch ${varpwd}/_docker/.server
-touch /code/docker_mysql_ports.json
+touch ${varpwd}/${DOCKER_FOLDER}/.active
+touch ${varpwd}/${DOCKER_FOLDER}/.server
+touch ${MYSQL_PORTS_FILE}
 
 # Log our current dev stack so we can shut it down later
-echo ${active_string} > ${varpwd}/_docker/.active
-echo ${server} > ${varpwd}/_docker/.server
+echo ${active_string} > ${varpwd}/${DOCKER_FOLDER}/.active
+echo ${server} > ${varpwd}/${DOCKER_FOLDER}/.server
 
-mysql_port=$(sed -n 's/.*"\([0-9]*\)":{"project":"'${project}'"}.*/\1/p' /code/docker_mysql_ports.json)
-
+mysql_port=$(sed -n 's/.*"\([0-9]*\)":{"project":"'${project}'"}.*/\1/p' ${MYSQL_PORTS_FILE})
 
 # No mysql_port found so let's figure out which to use
 if [[ ! -z $mysql_port ]]
@@ -339,7 +350,7 @@ else
 	echo "No existing MySQL Port found."
 	echo "Looking for next available port."
 
-	ports_string=$(</code/docker_mysql_ports.json)
+	ports_string=$(<${MYSQL_PORTS_FILE})
 	IFS=,
 	ary=($ports_string)
 
@@ -364,10 +375,8 @@ else
 
 	mysql_port=$[mysql_port + 1]
 
-	echo "Using MySQL Port: ${mysql_port}"
-
 	# Add our new port to the ports listing.
-	sed -i '' "s#}}}#},\"${mysql_port}\":{\"project\":\"${project}\"}}}#g" /code/docker_mysql_ports.json
+    sed -i '' "s#}}}#},\"${mysql_port}\":{\"project\":\"${project}\"}}}#g" ${MYSQL_PORTS_FILE}
 fi
 
 echo "---------------------------------------------"
@@ -381,69 +390,69 @@ echo "  database: ${project}"
 
 
 # Copy our PHP ini overrides ONLY if they don't already exist (so we don't override custom settings)
-if [[ ! -f ${varpwd}/_docker/php-ini-overrides.ini ]]
+if [[ ! -f ${varpwd}/${DOCKER_FOLDER}/php-ini-overrides.ini ]]
 then
-    cp -f /code/docker/_source/php-ini-overrides.ini ${varpwd}/_docker/php-ini-overrides.ini
+    cp -f /code/docker/_source/php-ini-overrides.ini ${varpwd}/${DOCKER_FOLDER}/php-ini-overrides.ini
 fi
 
 # If we're using ngrok, add the ngrok subdomain into our virtual_hosts
 if [[ $use_ngrok == 'y' || $use_ngrok == 'e' ]]
 then
-    virtual_hosts="${project}.dev,${project}.${tld},${project}.localhost,${ngrok_id}.ngrok.io"
+    virtual_hosts="${project}.dev,${project}.${TLD},${project}.localhost,${ngrok_id}.ngrok.io"
 else
-    virtual_hosts="${project}.dev,${project}.${tld},${project}.localhost"
+    virtual_hosts="${project}.dev,${project}.${TLD},${project}.localhost"
 fi
 
 # Create our docker-compose.yml file if it doesn't exist.
-cp -f /code/docker/_source/docker-compose-${server}.yml ${varpwd}/_docker/docker-compose.yml
+cp -f /code/docker/_source/docker-compose-${server}.yml ${varpwd}/${DOCKER_FOLDER}/docker-compose.yml
 
 # Create our Dockerfile
 # If the user has a custom Dockerfile, use that instead of the default one.
-if [[ -f ${varpwd}/_docker/Dockerfile-php${php_version}-custom ]]
+if [[ -f ${varpwd}/${DOCKER_FOLDER}/Dockerfile-php${php_version}-custom ]]
 then
     echo "${bold}Dockerfile-php${php_version}-custom found, using${normal}"
-    cp -f ${varpwd}/_docker/Dockerfile-php${php_version}-custom ${varpwd}/_docker/Dockerfile
-elif [[ -f ${varpwd}/_docker/Dockerfile-custom ]]
+    cp -f ${varpwd}/${DOCKER_FOLDER}/Dockerfile-php${php_version}-custom ${varpwd}/${DOCKER_FOLDER}/Dockerfile
+elif [[ -f ${varpwd}/${DOCKER_FOLDER}/Dockerfile-custom ]]
 then
     echo "${bold}Dockerfile-custom found, using${normal}"
-    cp -f ${varpwd}/_docker/Dockerfile-custom ${varpwd}/_docker/Dockerfile
+    cp -f ${varpwd}/${DOCKER_FOLDER}/Dockerfile-custom ${varpwd}/${DOCKER_FOLDER}/Dockerfile
 else
-    cp -f /code/docker/_source/Dockerfile-${server} ${varpwd}/_docker/Dockerfile
+    cp -f /code/docker/_source/Dockerfile-${server} ${varpwd}/${DOCKER_FOLDER}/Dockerfile
 fi
 
 # Replace the variables in our file with the stack we want to run.
-sed -i '' "s#@@@PROJECT@@@#${project}#g" ${varpwd}/_docker/docker-compose.yml
-sed -i '' "s#@@@PROJECT_PATH@@@#/code/sites/${project}#g" ${varpwd}/_docker/docker-compose.yml
-sed -i '' "s#@@@PHP_VERSION@@@#${php_version}#g" ${varpwd}/_docker/docker-compose.yml
-sed -i '' "s#@@@MYSQL_VERSION@@@#${mysql_version}#g" ${varpwd}/_docker/docker-compose.yml
-sed -i '' "s#@@@MYSQL_PORT@@@#${mysql_port}#g" ${varpwd}/_docker/docker-compose.yml
-sed -i '' "s#@@@VIRTUAL_HOSTS@@@#${virtual_hosts}#g" ${varpwd}/_docker/docker-compose.yml
-sed -i '' "s#@@@PHP_INI_FOLDER@@@#${php_ini_folder}#g" ${varpwd}/_docker/docker-compose.yml
-sed -i '' "s#@@@PROJECT_PATH@@@#/code/sites/${project}#g" ${varpwd}/_docker/Dockerfile
+sed -i '' "s#@@@PROJECT@@@#${project}#g" ${varpwd}/${DOCKER_FOLDER}/docker-compose.yml
+sed -i '' "s#@@@PROJECT_PATH@@@#${SITES_FOLDER}${project}#g" ${varpwd}/${DOCKER_FOLDER}/docker-compose.yml
+sed -i '' "s#@@@PHP_VERSION@@@#${php_version}#g" ${varpwd}/${DOCKER_FOLDER}/docker-compose.yml
+sed -i '' "s#@@@MYSQL_VERSION@@@#${mysql_version}#g" ${varpwd}/${DOCKER_FOLDER}/docker-compose.yml
+sed -i '' "s#@@@MYSQL_PORT@@@#${mysql_port}#g" ${varpwd}/${DOCKER_FOLDER}/docker-compose.yml
+sed -i '' "s#@@@VIRTUAL_HOSTS@@@#${virtual_hosts}#g" ${varpwd}/${DOCKER_FOLDER}/docker-compose.yml
+sed -i '' "s#@@@PHP_INI_FOLDER@@@#${php_ini_folder}#g" ${varpwd}/${DOCKER_FOLDER}/docker-compose.yml
+sed -i '' "s#@@@PROJECT_PATH@@@#${SITES_FOLDER}${project}#g" ${varpwd}/${DOCKER_FOLDER}/Dockerfile
 
 if [[ $server == 'apache' ]]
 then
     # Copy our apache.conf file
     if [[ -f /code/docker/_source/apache.conf ]]
     then
-        cp -f /code/docker/_source/apache.conf ${varpwd}/_docker/apache.conf
+        cp -f /code/docker/_source/apache.conf ${varpwd}/${DOCKER_FOLDER}/apache.conf
     fi
 
     # If the user has a custom apache config file, use that instead of the default one.
-    if [[ -f ${varpwd}/_docker/apache-php${php_version}-custom.conf ]]
+    if [[ -f ${varpwd}/${DOCKER_FOLDER}/apache-php${php_version}-custom.conf ]]
     then
         apache_conf_file="apache-php${php_version}-custom"
-    elif [[ -f ${varpwd}/_docker/apache-custom.conf ]]
+    elif [[ -f ${varpwd}/${DOCKER_FOLDER}/apache-custom.conf ]]
     then
         apache_conf_file="apache-custom"
     else
         apache_conf_file="apache"
     fi
 
-    sed -i '' "s#@@@APACHE_CONF_FILE@@@#${apache_conf_file}#g" ${varpwd}/_docker/docker-compose.yml
-    sed -i '' "s#@@@PROJECT_PATH_SERVER@@@#/code/sites/${project}#g" ${varpwd}/_docker/apache.conf
-    sed -i '' "s#@@@PROJECT_PATH_SERVER@@@#/code/sites/${project}#g" ${varpwd}/_docker/docker-compose.yml
-    sed -i '' "s#@@@PROJECT_PATH_SERVER@@@#/code/sites/${project}#g" ${varpwd}/_docker/Dockerfile
+    sed -i '' "s#@@@APACHE_CONF_FILE@@@#${apache_conf_file}#g" ${varpwd}/${DOCKER_FOLDER}/docker-compose.yml
+    sed -i '' "s#@@@PROJECT_PATH_SERVER@@@#${SITES_FOLDER}${project}#g" ${varpwd}/${DOCKER_FOLDER}/apache.conf
+    sed -i '' "s#@@@PROJECT_PATH_SERVER@@@#${SITES_FOLDER}${project}#g" ${varpwd}/${DOCKER_FOLDER}/docker-compose.yml
+    sed -i '' "s#@@@PROJECT_PATH_SERVER@@@#${SITES_FOLDER}${project}#g" ${varpwd}/${DOCKER_FOLDER}/Dockerfile
 
     # Replace the variables in our Dockerfile with the stack we want to run.
     for index in "${php_apache_repos[@]}" ; do
@@ -455,37 +464,37 @@ then
             repo_repo="${repo_string%%;;*}"
             repo_tag="${repo_string##*;;}"
 
-            sed -i '' "s#@@@PHP_REPO@@@#${repo_repo}#g" ${varpwd}/_docker/Dockerfile
-            sed -i '' "s#@@@PHP_REPO_TAG@@@#${repo_tag}#g" ${varpwd}/_docker/Dockerfile
-            sed -i '' "s#@@@PHP_REPO@@@#${repo_repo}#g" ${varpwd}/_docker/docker-compose.yml
-            sed -i '' "s#@@@PHP_REPO_TAG@@@#${repo_tag}#g" ${varpwd}/_docker/docker-compose.yml
+            sed -i '' "s#@@@PHP_REPO@@@#${repo_repo}#g" ${varpwd}/${DOCKER_FOLDER}/Dockerfile
+            sed -i '' "s#@@@PHP_REPO_TAG@@@#${repo_tag}#g" ${varpwd}/${DOCKER_FOLDER}/Dockerfile
+            sed -i '' "s#@@@PHP_REPO@@@#${repo_repo}#g" ${varpwd}/${DOCKER_FOLDER}/docker-compose.yml
+            sed -i '' "s#@@@PHP_REPO_TAG@@@#${repo_tag}#g" ${varpwd}/${DOCKER_FOLDER}/docker-compose.yml
         fi
     done
 else
     # Copy our nginx.conf file
-    cp -f /code/docker/_source/nginx.conf ${varpwd}/_docker/nginx.conf
+    cp -f /code/docker/_source/nginx.conf ${varpwd}/${DOCKER_FOLDER}/nginx.conf
 
     # If the user has a custom nginx config file, use that instead of the default one.
-    if [[ -f ${varpwd}/_docker/nginx-php${php_version}-custom.conf ]]
+    if [[ -f ${varpwd}/${DOCKER_FOLDER}/nginx-php${php_version}-custom.conf ]]
     then
         nginx_conf_file="nginx-php${php_version}-custom"
-        echo "${bold}nginx-php${php_version}-custom.conf found, using${normal}"
-    elif [[ -f ${varpwd}/_docker/nginx-custom.conf ]]
+        echo "NGINX: ${bold}nginx-php${php_version}-custom.conf${normal}"
+    elif [[ -f ${varpwd}/${DOCKER_FOLDER}/nginx-custom.conf ]]
     then
         nginx_conf_file="nginx-custom"
-        echo "${bold}nginx-custom.conf found, using${normal}"
+        echo "NGINX: ${bold}nginx-custom.conf${normal}"
     else
         nginx_conf_file="nginx"
-        echo "using default nginx.conf"
+        echo "NGINX: ${bold}nginx.conf${normal}"
     fi
 
-    sed -i '' "s#@@@NGINX_FILE@@@#${nginx_conf_file}#g" ${varpwd}/_docker/docker-compose.yml
-    sed -i '' "s#@@@PROJECT@@@#${project}#g" ${varpwd}/_docker/nginx.conf
-    sed -i '' "s#@@@PROJECT_PUBLIC@@@#${public_folder_path}#g" ${varpwd}/_docker/nginx.conf
-    sed -i '' "s#@@@PHP_VERSION@@@#${php_version}#g" ${varpwd}/_docker/nginx.conf
-    sed -i '' "s#@@@TLD@@@#${tld}#g" ${varpwd}/_docker/nginx.conf
-    sed -i '' "s#@@@PROJECT_PATH_SERVER@@@#/code/sites/${project}#g" ${varpwd}/_docker/docker-compose.yml
-    sed -i '' "s#@@@PROJECT_PATH_SERVER@@@#/code/sites/${project}#g" ${varpwd}/_docker/Dockerfile
+    sed -i '' "s#@@@NGINX_FILE@@@#${nginx_conf_file}#g" ${varpwd}/${DOCKER_FOLDER}/docker-compose.yml
+    sed -i '' "s#@@@PROJECT@@@#${project}#g" ${varpwd}/${DOCKER_FOLDER}/nginx.conf
+    sed -i '' "s#@@@PROJECT_PUBLIC@@@#${public_folder_path}#g" ${varpwd}/${DOCKER_FOLDER}/nginx.conf
+    sed -i '' "s#@@@PHP_VERSION@@@#${php_version}#g" ${varpwd}/${DOCKER_FOLDER}/nginx.conf
+    sed -i '' "s#@@@TLD@@@#${TLD}#g" ${varpwd}/${DOCKER_FOLDER}/nginx.conf
+    sed -i '' "s#@@@PROJECT_PATH_SERVER@@@#${SITES_FOLDER}${project}#g" ${varpwd}/${DOCKER_FOLDER}/docker-compose.yml
+    sed -i '' "s#@@@PROJECT_PATH_SERVER@@@#${SITES_FOLDER}${project}#g" ${varpwd}/${DOCKER_FOLDER}/Dockerfile
 
     # Replace the variables in our Dockerfile with the stack we want to run.
     for index in "${php_repos[@]}" ; do
@@ -497,8 +506,8 @@ else
             repo_repo="${repo_string%%;;*}"
             repo_tag="${repo_string##*;;}"
 
-            sed -i '' "s#@@@PHP_REPO@@@#${repo_repo}#g" ${varpwd}/_docker/Dockerfile
-            sed -i '' "s#@@@PHP_REPO_TAG@@@#${repo_tag}#g" ${varpwd}/_docker/Dockerfile
+            sed -i '' "s#@@@PHP_REPO@@@#${repo_repo}#g" ${varpwd}/${DOCKER_FOLDER}/Dockerfile
+            sed -i '' "s#@@@PHP_REPO_TAG@@@#${repo_tag}#g" ${varpwd}/${DOCKER_FOLDER}/Dockerfile
         fi
     done
 fi
@@ -510,7 +519,7 @@ for index in "${php_extensions[@]}" ; do
     then
         repo_extensions="${index##*::}"
 
-        sed -i '' "s#@@@PHP_EXTENSIONS@@@#${repo_extensions}#g" ${varpwd}/_docker/Dockerfile
+        sed -i '' "s#@@@PHP_EXTENSIONS@@@#${repo_extensions}#g" ${varpwd}/${DOCKER_FOLDER}/Dockerfile
     fi
 done
 
@@ -519,45 +528,47 @@ echo "Launching New Stack: PHP ${php_version} / MySQL ${mysql_version}"
 echo "---------------------------------------------"
 
 # If the user has a custom docker-compose file, use that instead of the default one.
-if [[ -f ${varpwd}/_docker/docker-compose-php${php_version}-custom.yml ]]
+if [[ -f ${varpwd}/${DOCKER_FOLDER}/docker-compose-php${php_version}-custom.yml ]]
 then
     docker_compose_file="docker-compose-php${php_version}-custom"
-    echo "${bold}docker-compose-php${php_version}-custom.yml found, using${normal}"
-elif [[ -f ${varpwd}/_docker/docker-compose-custom.yml ]]
+    echo "Compose: ${bold}docker-compose-php${php_version}-custom.yml${normal}"
+elif [[ -f ${varpwd}/${DOCKER_FOLDER}/docker-compose-custom.yml ]]
 then
     docker_compose_file="docker-compose-custom"
-    echo "${bold}docker-compose-custom.yml found, using${normal}"
+    echo "Compose: ${bold}docker-compose-custom.yml${normal}"
 else
     docker_compose_file="docker-compose"
-    echo "using default docker-compose.yml"
+    echo "Compose: ${bold}docker-compose.yml${normal}"
 fi
 
 # Launch our new dev stack
-docker-compose -p '${project}' -f ${varpwd}/_docker/${docker_compose_file}.yml up -d #> /dev/null 2>&1
+docker-compose -p '${project}' -f ${varpwd}/${DOCKER_FOLDER}/${docker_compose_file}.yml up -d #> /dev/null 2>&1
 
 # If we have custom commands to run in the container afterward, do so.
-if [[ -f ${varpwd}/_docker/docker-post-launch-php${php_version}.sh ]]
+if [[ -f ${varpwd}/${DOCKER_FOLDER}/docker-post-launch-php${php_version}.sh ]]
 then
-    echo "${bold}docker-post-launch-php${php_version}.sh found, using${normal}"
-    chmod +x ${varpwd}/_docker/docker-post-launch-php${php_version}.sh
-    ${varpwd}/_docker/docker-post-launch-php${php_version}.sh
-elif [[ -f ${varpwd}/_docker/docker-post-launch.sh ]]
+    echo "Post-Launch PHP: ${bold}docker-post-launch-php${php_version}.sh${normal}"
+    chmod +x ${varpwd}/${DOCKER_FOLDER}/docker-post-launch-php${php_version}.sh
+    ${varpwd}/${DOCKER_FOLDER}/docker-post-launch-php${php_version}.sh
+elif [[ -f ${varpwd}/${DOCKER_FOLDER}/docker-post-launch.sh ]]
 then
-    echo "${bold}docker-post-launch.sh found, using${normal}"
-    chmod +x ${varpwd}/_docker/docker-post-launch.sh
-    ${varpwd}/_docker/docker-post-launch.sh
-    #exec_commands=$(<${varpwd}/_docker/docker-post-launch.sh)
+    echo "Post-Launch PHP: ${bold}docker-post-launch.sh${normal}"
+    chmod +x ${varpwd}/${DOCKER_FOLDER}/docker-post-launch.sh
+    ${varpwd}/${DOCKER_FOLDER}/docker-post-launch.sh
+    #exec_commands=$(<${varpwd}/${DOCKER_FOLDER}/docker-post-launch.sh)
     #docker_command="docker exec ${project}-${server} ${exec_commands}"
     #eval $docker_command
 fi
 
-echo "Stack Launched!"
-echo "http://${project}.${tld}/"
+echo "---------------------------------------------"
+echo "${bold}Stack Launched!${normal}"
+echo "URL: ${bold}http://${project}.${TLD}/${normal}"
+echo "MySQL Port: ${bold}${mysql_port}${normal}"
 echo "---------------------------------------------"
 printf "\n"
 
 # Rewrite the database connection settings
-cat > ${varpwd}/_docker/docker.database.php <<- DatabaseContent
+cat > ${varpwd}/${DOCKER_FOLDER}/docker.database.php <<- DatabaseContent
 <?php
 // EE2 Format
 \$db['expressionengine']['hostname'] = '${project}-mysql${mysql_version}';
@@ -571,9 +582,9 @@ cat > ${varpwd}/_docker/docker.database.php <<- DatabaseContent
 \$config['database']['expressionengine']['password'] = 'root_password';
 \$config['database']['expressionengine']['database'] = '${project}';
 
-\$config['base_url'] = 'http://${project}.${tld}/';
-\$config['site_url'] = 'http://${project}.${tld}/';
-\$config['base_path'] = '/code/sites/${project}/';
+\$config['base_url'] = 'http://${project}.${TLD}/';
+\$config['site_url'] = 'http://${project}.${TLD}/';
+\$config['base_path'] = '${SITES_FOLDER}${project}/';
 
 if (file_exists(\$config['base_path'].'themes')) {
     \$config['theme_folder_url'] = \$config['base_url'].'themes/';
